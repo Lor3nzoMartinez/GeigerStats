f1c4c7724d779627f9e93cf37e8d0692d4d42c86
Lorenzo <Lorenzo.s.martinez0@gmail.com>
2018-11-17 12:10:59 -0800

    CSVReader Upadted

--- GygerThing/src/CSVReader.java
+++ GygerThing/src/CSVReader.java
@@ -1,9 +1,25 @@
+import java.util.*;
+import java.io.*;
+
 
 public class CSVReader {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hii");
+		//Determine our CSV file and parse output into java
+		String fileName = ("C:\\Users\\Lorenzo\\Desktop\\141 Project Geiger\\GygerThing\\Supp Docs\\7_04_18.csv");
+		File file = new File(fileName);
+		try {
+			Scanner inputStream = new Scanner(file);
+			while(inputStream.hasNext()) {
+				String data = inputStream.next();
+				System.out.println(data);
+				
+			}
+		} 
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("I don't see anything!");;
+		}
 
 	}
 
