ee1717b5e8da2782862c41fc964ceed60f9a7931
Lorenzo <Lorenzo.s.martinez0@gmail.com>
2018-11-17 22:28:24 -0800

    CSVReader has been completed.

--- GygerThing/src/CSVReader.java
+++ GygerThing/src/CSVReader.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.math.BigInteger;
 
 
 public class CSVReader {
@@ -13,32 +12,30 @@ public class CSVReader {
 		
 		try {
 			Scanner inputStream = new Scanner(file);
-			ArrayList listOfCounts = new ArrayList();
+			ArrayList listOfCounts = new ArrayList(102317);
+			ArrayList listOfDates = new ArrayList(102317);
+			ArrayList listOfTimes = new ArrayList(102317);
+			ArrayList listOfOccurance = new ArrayList(102317);
 			int total = 0;		//init Values
 			int counter = 0;	//init Values
 			while(inputStream.hasNext()) {
 				String data = inputStream.nextLine();
 				String[] values = data.split(",");
-				String date = values[0];
-				String time = values[1];
-				String howOften = values[2];
-				double CountsPerMinute = Double.parseDouble(values[3]);
+				listOfDates.add(values[0]);
+				listOfTimes.add(values[1]);
+				listOfOccurance.add(values[2]);
 				listOfCounts.add(values[3]);
+				double CountsPerMinute = Double.parseDouble(values[3]);
 				total += CountsPerMinute;
 				counter++;
 			}
 		inputStream.close();
-		Collections.sort( listOfCounts, null);
-		int[] listOfNumbs = listOfCounts.();
-			
-				
-			
-		} 
-		catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println("I don't see anything!");;
+		//System.out.print(noDupes);
 		}
-	}
+		
+		catch (FileNotFoundException e) {
+			System.out.println("Wrong file path!");
+		}
+	}	
 }
 	
-
