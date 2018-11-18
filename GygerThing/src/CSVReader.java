33549926e52c58606d2534be0e432fa65a702691
Lorenzo <Lorenzo.s.martinez0@gmail.com>
2018-11-17 20:49:59 -0800

    Idk. Just pushing incase you know...

--- GygerThing/src/CSVReader.java
+++ GygerThing/src/CSVReader.java
@@ -5,19 +5,20 @@ import java.math.BigInteger;
 
 public class CSVReader {
 
-	public static <CountsPerMinute> void main(String[] args) {
+	@SuppressWarnings({ "unchecked", "unused" })
+	public static void main(String[] args) {
 		//Determine our CSV file and parse output into java
 		String fileName = ("C:\\Users\\Lorenzo\\Desktop\\141 Project Geiger\\GygerThing\\Supp Docs\\7_04_18.csv");
 		File file = new File(fileName);
 		
 		try {
 			Scanner inputStream = new Scanner(file);
-			ArrayList<CountsPerMinute> listOfCounts = new ArrayList();
+			ArrayList listOfCounts = new ArrayList();
 			int total = 0;		//init Values
 			int counter = 0;	//init Values
 			while(inputStream.hasNext()) {
 				String data = inputStream.nextLine();
-				CountsPerMinute values = data.split(",");
+				String[] values = data.split(",");
 				String date = values[0];
 				String time = values[1];
 				String howOften = values[2];
@@ -25,22 +26,12 @@ public class CSVReader {
 				listOfCounts.add(values[3]);
 				total += CountsPerMinute;
 				counter++;
-				
-	
 			}
-			System.out.print(listOfCounts);
-			inputStream.close();
-			System.out.println("Average is "+ (total / counter));
+		inputStream.close();
+		Collections.sort( listOfCounts, null);
+		int[] listOfNumbs = listOfCounts.();
 			
-			
-			
-			
-			
-			
-			/*for (int counter = 0; inputStream.hasNext(); counter++)
-				System.out.println(data);*/
-			
-					
+				
 			
 		} 
 		catch (FileNotFoundException e) {
