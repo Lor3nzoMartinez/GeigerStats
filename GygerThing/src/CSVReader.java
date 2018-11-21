8af88bec9b81ba54ea488f7b7ed7d83cca3221fe
Lorenzo <Lorenzo.s.martinez0@gmail.com>
2018-11-20 23:11:58 -0800

    Finnished main objective. Now working on extcredit/ might need to reformat csvReader

--- GygerThing/src/CSVReader.java
+++ GygerThing/src/CSVReader.java
@@ -4,13 +4,10 @@ import java.io.*;
 public class CSVReader {
 
 	public static void main(String[] args) {
-		//Determine our CSV file and parse output into java
 		String fileName = ("C:\\Users\\Lorenzo\\Desktop\\141 Project Geiger\\GygerThing\\Supp Docs\\7_04_18.csv");
 		File file = new File(fileName);
-		//Declare array list to store data
 		ArrayList<Integer> listOfCounts = new ArrayList();
 		ArrayList<String> listOfDates = new ArrayList();
-		
 		try {
 			Scanner inputStream = new Scanner(file);
 			while(inputStream.hasNext()) {
@@ -26,39 +23,30 @@ public class CSVReader {
 		catch (FileNotFoundException e) {
 			System.out.println("Wrong file path!");
 		}
-		//display(listOfDates, listOfCounts);
-		//highestCPM(listOfCounts);
-		withinFive(listOfCounts);
-	}
-	public static void display(ArrayList date, ArrayList cpm) {
-		for (int i = 0; i < cpm.size(); ++i)
-			System.out.println(date.get(i) + " " + cpm.get(i));
-			
-	}
-	public static void highestCPM(ArrayList<Integer> cpm){
-		int highest = 0;
-		for (int counter = 0; counter < cpm.size(); ++counter)
-			if (cpm.get(counter) > highest)
-				highest = cpm.get(counter);
-		System.out.println("Highest value in dataset is: " + highest);	
-					
+		withinFive(listOfCounts, listOfDates);
+		//graph(listOfCounts,listOfDates);
 	}
 	public static void withinFive(ArrayList<Integer> cpm, ArrayList<String> dates){
 		ArrayList<Integer> listOfCounts = new ArrayList();
-		String[] values = null;
 		int highest = 0;
 		for (int counter = 0; counter < cpm.size(); ++counter)
 			if (cpm.get(counter) > highest)
 				highest = cpm.get(counter);
 		for (int counter = 0; counter < cpm.size(); ++counter)
 			for (int helper = 0; helper <= 5; ++helper)
-				if (cpm.get(counter) == highest-helper) 
-					listOfCounts.add(cpm.get(counter));
-		System.out.println("Date           Count");
-		System.out.println("--------------------");
+				if (cpm.get(counter) == highest-helper) {
+					listOfCounts.add(counter);
+				}	
+		System.out.println("Date                  Count");
+		System.out.println("---------------------------");
 		for (int x = 0; x < listOfCounts.size(); x++)
-			System.out.println("                 "+listOfCounts.get(x));	
+			System.out.println(dates.get(listOfCounts.get(x))+ "               " +cpm.get(listOfCounts.get(x)));	
 					
+	}
+	public static void graph(ArrayList cpm, ArrayList date) {                 // work onme
+		System.out.print(cpm.size());
+		for (int x = 0; x < date.size(); x++)
+			System.out.print(x);
 	}
 }
 
